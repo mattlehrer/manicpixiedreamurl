70b27e827311fecad1e56f0f575c448d397e2805
Matt Lehrer <lehrerm@gmail.com>
2023-12-05 14:27:49 +0100

    setup open-props

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import 'open-props/style';
+	import 'open-props/normalize';
+	import 'open-props/buttons';
+
+	import '../app.css';
+</script>
+
+<div>
+	<main>
+		<slot />
+	</main>
+</div>

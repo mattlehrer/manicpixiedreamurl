7b244ed9432de2303a76027489cec37aef7531fa
Matt Lehrer <lehrerm@gmail.com>
2023-12-07 11:07:54 +0100

    sqlite issues

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,8 @@
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import sqlite from 'better-sqlite3';
 
-export const sqliteDatabase = sqlite('sqlite.db');
+export const sqliteDatabase = new sqlite('sqlite.db', {
+	verbose: console.log,
+	fileMustExist: false,
+});
 export const db: BetterSQLite3Database = drizzle(sqliteDatabase);

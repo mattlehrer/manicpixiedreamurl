c878b1350d8186cd26a3118179da23d7d23863c2
Matt Lehrer <lehrerm@gmail.com>
2023-12-07 11:20:53 +0100

    use new node: style

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,7 @@
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import sqlite from 'better-sqlite3';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export const sqliteDatabase = new sqlite(dirname(fileURLToPath(import.meta.url)) + '/../../../sqlite.db', {
 	verbose: console.log,

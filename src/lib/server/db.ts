01d51d2cf709afd724c55161efee4307e028d339
Matt Lehrer <lehrerm@gmail.com>
2023-12-07 11:16:25 +0100

    use full path

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,9 @@
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import sqlite from 'better-sqlite3';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export const sqliteDatabase = new sqlite('sqlite.db', {
+export const sqliteDatabase = new sqlite(dirname(fileURLToPath(import.meta.url)) + '/../../../sqlite.db', {
 	verbose: console.log,
 	fileMustExist: false,
 });

8c9164b45c6fec57b2ecc570b69d00db7d9bf63b
Matt Lehrer <lehrerm@gmail.com>
2023-12-07 23:05:38 +0100

    implement caddy check

--- src/routes/api/caddy/check/+server.ts
+++ src/routes/api/caddy/check/+server.ts
@@ -1,6 +1,22 @@
+import { domain } from '$lib/schema';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-	// TODO: implement db check
-	return new Response();
+export const GET: RequestHandler = async ({ url }) => {
+	const toCheck = url.searchParams.get('domain');
+	if (!toCheck) {
+		return new Response('', { status: 400 });
+	}
+
+	const exists = await db
+		.select({ name: domain.name, isActive: domain.isActive })
+		.from(domain)
+		.where(eq(domain.name, toCheck));
+
+	if (exists && exists.length > 0 && exists[0].isActive) {
+		return new Response();
+	} else {
+		return new Response('', { status: 404 });
+	}
 };

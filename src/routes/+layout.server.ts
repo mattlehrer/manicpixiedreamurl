084bf002d508e39f6c1637ac5be34a2e7422ccd8
Matt Lehrer <lehrerm@gmail.com>
2023-12-08 11:18:07 +0100

    redirect to / on network sites (for now?)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,17 @@
+import { dashboardSite } from '$lib/config';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
+	const host = url.host;
+	if (host !== dashboardSite && !['/'].includes(url.pathname)) {
+		throw redirect(303, '/');
+	}
+
 	const session = await locals.auth?.validate();
 
 	return {
-		host: url.host,
+		host,
 		username: session?.user.username,
 	};
 };

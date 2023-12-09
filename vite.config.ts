6f744dfa0ce76e7c3b0bf970db94a37f006ddc80
Matt Lehrer <lehrerm@gmail.com>
2023-12-09 11:06:32 +0100

    setup https locally with a couple test domains

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	server: {
+		https: true,
+	},
+	plugins: [
+		mkcert({
+			hosts: ['localhost', 'a.test', 'b.test'],
+		}),
+		sveltekit(),
+	],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
 });

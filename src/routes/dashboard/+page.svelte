42ddd2f3579083a2f9ecfefdae1666730dbb08f5
Matt Lehrer <lehrerm@gmail.com>
2023-12-14 15:43:53 +0100

    setup domains table and allow editing reason

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -9,6 +9,24 @@
 
 	let reason: string = form ? String(form?.reason) : '';
 	let domain: string = form ? String(form?.domain) : '';
+
+	let updating: Record<string, 'updating' | 'updated' | 'error'> = {};
+
+	async function updateReason(domainId: string, domainReason: string) {
+		updating[domainId] = 'updating';
+		const res = await fetch(`/api/domain`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ domainId, domainReason }),
+		});
+		if (res.ok) {
+			updating[domainId] = 'updated';
+		} else {
+			updating[domainId] = 'error';
+		}
+	}
 </script>
 
 <h1>Welcome {data.username}</h1>
@@ -41,30 +59,118 @@
 
 <h2>Domains</h2>
 <p>You are currently using {data.domains.length} out of {data.maxDomains} domains on your current plan.</p>
-<ul>
-	{#each data.domains as domain}
-		<li>
-			<a href="https://{domain.name}">{domain.name}</a> because {domain.reason}
-			<!-- <form method="post" action="?/removeDomain" use:enhance>
-				<input type="hidden" name="domain" value={domain} />
-				<input type="submit" value="Remove" />
-			</form> -->
-		</li>
-	{/each}
-</ul>
+<table id="domains-table">
+	<thead>
+		<tr>
+			<th>Domain</th>
+			<th class="wider">Reason</th>
+			<!-- <th>Remove</th> -->
+		</tr>
+	</thead>
+	<tbody>
+		{#each data.domains as domain}
+			<tr>
+				<td>
+					<a href="https://{domain.name}">{domain.name}</a>
+				</td>
 
-<style>
+				<td class="wider">
+					<textarea
+						name="{domain.name}-reason"
+						rows="3"
+						bind:value={domain.reason}
+						onchange={() => updateReason(domain.id, domain.reason)}
+					/>
+					{#if updating[domain.id] === 'updating'}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							style="color: var(--text-2);"><path d="M21 12a9 9 0 1 1-6.219-8.56" /></svg
+						>
+						<span class="sr-only">Updating...</span>
+					{:else if updating[domain.id] === 'error'}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							style="color: var(--red-8);"
+							><circle cx="12" cy="12" r="10" /><path d="m15 9-6 6" /><path d="m9 9 6 6" /></svg
+						>
+						<span class="sr-only">Error</span>
+					{:else if updating[domain.id] === 'updated'}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							style="color: var(--green-7);"><circle cx="12" cy="12" r="10" /><path d="m9 12 2 2 4-4" /></svg
+						>
+						<span class="sr-only">Updated</span>
+					{/if}
+				</td>
+				<!-- <td>
+					<form method="post" action="?/removeDomain" use:enhance>
+						<input type="hidden" name="domain" value={domain.name} />
+						<input type="submit" value="Remove" />
+					</form>
+				</td> -->
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style lang="postcss">
 	label {
 		display: block;
 		margin-block-start: var(--size-2);
 	}
-	ul {
+	table {
 		margin-block-start: var(--size-5);
+		width: 100%;
 	}
 
-	li {
-		margin-block-start: var(--size-2);
-		font-size: var(--font-size-fluid-1);
+	td > form {
+		padding: 0;
+		margin: 0;
+	}
+
+	th:first-child,
+	td:first-child {
+		width: 8rem;
+		text-align: left;
+		padding-inline: var(--size-1);
+	}
+
+	.wider {
+		min-width: 100%;
+		text-align: left;
+		padding-inline: var(--size-2);
+		display: flex;
+		align-items: center;
+		gap: var(--size-2);
+	}
+
+	.wider textarea {
+		margin-inline-start: calc(-1 * var(--size-2));
+		width: 85%;
 	}
 
 	.reason {

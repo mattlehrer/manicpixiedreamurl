83adfe667181e77cffca78981e31f1968c8040ca
Matt Lehrer <lehrerm@gmail.com>
2023-12-15 09:06:12 +0100

    fix relative path

--- migrate.ts
+++ migrate.ts
@@ -1,15 +1,17 @@
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import sqlite from 'better-sqlite3';
+import { dev } from '$app/environment';
 
 console.log('Running migrations...');
-const sqlitePath = new URL('./sqlite.db', import.meta.url).pathname;
+const sqlitePath = new URL(dev ? '' : '../../..' + './sqlite.db', import.meta.url).pathname;
 console.log({ sqlitePath });
 const sqliteDatabase = new sqlite(sqlitePath);
 const db: BetterSQLite3Database = drizzle(sqliteDatabase);
 
 try {
-	migrate(db, { migrationsFolder: new URL('./drizzle/migrations', import.meta.url).pathname });
+	const migrationsPath = new URL(dev ? '' : '../../..' + './drizzle/migrations', import.meta.url).pathname;
+	migrate(db, { migrationsFolder: migrationsPath });
 } catch (error) {
 	console.error(error);
 	if (error instanceof Error) console.error(error.name, error.message);

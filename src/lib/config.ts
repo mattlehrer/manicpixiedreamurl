e3fca9c2bd89ebd1a8d755ab964d7b6d3362c0f4
Matt Lehrer <lehrerm@gmail.com>
2023-12-15 09:06:04 +0100

    add www. for dashboard

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment';
 
 export const dashboardSites = dev
 	? ['http://localhost:5173', 'http://192.168.0.243:5173', 'http://172.16.41.193:5173']
-	: ['https://manicpixiedreamurl.com'];
+	: ['https://manicpixiedreamurl.com', 'https://www.manicpixiedreamurl.com'];

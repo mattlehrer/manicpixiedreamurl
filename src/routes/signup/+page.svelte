e95859d87ee43fc5549d38ef4a40e4536c7900fb
Matt Lehrer <lehrerm@gmail.com>
2023-12-31 23:14:14 +0100

    add cloudflare turnstile to signup page

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import type { PageData, ActionData } from './$types';
+	import { PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY } from '$env/static/public';
 
 	export let form: ActionData;
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+</svelte:head>
+
 <h1>Sign up</h1>
 <form method="post" use:enhance>
+	{#if form?.captcha}<p class="error">Are you a bot? Please try again.</p>{/if}
 	{#if form?.invalidUsername}<p class="error">Your username should be between 3 and 30 characters.</p>{/if}
 	{#if form?.invalidEmail}<p class="error">Enter a valid email.</p>{/if}
 	{#if form?.duplicate}<p class="error">
@@ -21,6 +27,11 @@
 	<input name="email" id="email" /><br />
 	<label for="password">Password</label>
 	<input type="password" name="password" id="password" /><br />
+	<div
+		class="cf-turnstile"
+		data-appearance="interaction-only"
+		data-sitekey={PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
+	></div>
 	<input type="submit" />
 </form>
 <a href={data.loginLink}>Sign in</a>

fa582f4fe47bc36bc70b3f083d70a04e5a38873b
Matt Lehrer <lehrerm@gmail.com>
2024-01-20 14:00:19 +0100

    make properties optional

--- src/othertypes.d.ts
+++ src/othertypes.d.ts
@@ -2,8 +2,8 @@ declare namespace svelteHTML {
 	interface IntrinsicElements {
 		script: {
 			'event-site'?: string;
-			'event-logged_in': boolean;
-			src: string;
+			'event-logged_in'?: boolean;
+			src?: string;
 			async?: boolean;
 			defer?: boolean;
 			type?: string;
